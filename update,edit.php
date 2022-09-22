70c587ab834efc5c88e7fd7939018a99ae761ec1
chiky bum <108600649+unknown-man@users.noreply.github.com>
2022-09-23 03:47:52 +0600

    Update update,edit.php

--- update,edit.php
+++ update,edit.php
@@ -10,20 +10,16 @@ function update($table,$post,$files,$id){
   $r = count($post)-3;
   $p = array_keys($post);
   echo "UPDATE $table SET ";
-
   $file_num = count($files); ////  2 number of  inputs
   $file_key=array_keys($files);///array
-
   for ($i=0; $i <= $file_num-1; $i++) {
-
              $files1 = $files[$file_key[$i]];
              $files_name = $files1['name'];//i really dont know why i am doing this.....
              $tmp_name=$files1['tmp_name'];
              $file_key1 = array_keys($files_name);
              $file_num1 = count($file_key1);/////images per input
 if (!empty(trim($files_name[0]))) {
-                  if ($table=="plannar") {
-                    $get_prev_file=mysqli_query($conn,"SELECT * FROM plannar WHERE id ='$id'");
+                    $get_prev_file=mysqli_query($conn,"SELECT * FROM $table WHERE id ='$id'");
                     $got_files=mysqli_fetch_assoc($get_prev_file);
                      $got_files_str = ($got_files['file_name']);
                     $got_file =  (explode("hellopapa",$got_files_str));
@@ -32,9 +28,7 @@ if (!empty(trim($files_name[0]))) {
                       $got_file_name =  base64_decode($got_file[$v]);
                       unlink($upload.$got_file_name);
                     }
-                  }
              echo ($file_key[$i])." = '";
-
        for ($k=0; $k <= $file_num1 -1 ; $k++) {
          if (!empty(trim($files_name[$k]))) {
          $name=time().$k.$files_name[$k];///unique name
@@ -57,7 +51,6 @@ if (!empty(trim($files_name[0]))) {
     }
   echo " WHERE id = $id";
 }
-
 if (isset($_POST['edit'])) {
     $table=$_POST['table_name'];
     $id=$_POST['id'];
